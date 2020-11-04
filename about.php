b93b9b5df9394bddf56916e5edfb351e388cc5fd
Cogr397 <70345730+Cogr397@users.noreply.github.com>
2020-11-04 18:59:19 -0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,53 @@
+<?php 
+require 'includes/header.php'
+?>
+<main>
+<link rel="stylesheet" href="css/about.css">
+    <div class="about">
+        <h1>About Us</h1>
+        <p>Since 2015 we have been one of the most trusted game reviewers for gamers. </p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="Images/image.jpg">
+                <div class="container">
+                    <h2>Head Chef</h2>
+                    <p>Fry Cook</p>
+                    <p>Email: Fry@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/image 2.jpg">
+                <div class="container">
+                    <h2>Chief of Security</h2>
+                    <p>Unknown</p>
+                    <p>Email: Anonymous@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/image 3.png">
+                <div class="container">
+                    <h2>Head journalist</h2>
+                    <p>Fry Cook's twin</p>
+                    <p>Email: fury@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/image 3.jpg">
+                <div class="container">
+                    <h2>Owner</h2>
+                    <p>Alex Mason</p>
+                    <p>Email: Wutnumbers@cs230.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</main>
